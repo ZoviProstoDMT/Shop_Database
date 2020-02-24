7920d804e84734adf1990bd8c83a362fe0a5c0fd
Kozikov Dmitriy <kozikov.dmitrii@mail.ru>
2020-02-24 12:59:09 +0400

    Добавлен метод OpenNewScene
    Метод OpenNewScene упрощает открытие новых окон, освобождает код от многократного повторения и увеличивает читаемость.
    
    Добавлен переход на новую страницу при успешной авторизации

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/loginPage.fxml"));
         primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setResizable(false);
         primaryStage.setTitle("Shop Database");
         primaryStage.show();
     }
