2b017d7129d6b3eb69966beda128e474e6779402
Kozikov Dmitriy <kozikov.dmitrii@mail.ru>
2020-02-25 18:44:01 +0400

    Добавлены окна для Юзера и Суперюзера
    Теперь при авторизации осуществляется проверка роли, в связи с которой происходит открытие соответсвующего следующего окна

---
+++ src/sample/controllers/SuperuserAppController.java
@@ -0,0 +1,45 @@
+package sample.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class SuperuserAppController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label SuperuserRoleTextLabel;
+
+    @FXML
+    private Button SuperuserCustomers;
+
+    @FXML
+    private Button SuperuserOrders;
+
+    @FXML
+    private Button SuperuserProducts;
+
+    @FXML
+    private Label SuperuserRoleTextLabel1;
+
+    @FXML
+    private Label SuperuserRoleTextLabel11;
+
+    @FXML
+    private Button SuperuserUsers;
+
+    @FXML
+    private Button SuperuserExitButton;
+
+    @FXML
+    void initialize() {
+
+    }
+}
