115737e04ee8f8069e4fe8c5ff1c76d3f04a6c30
Kozikov Dmitriy <kozikov.dmitrii@mail.ru>
2020-02-26 10:31:50 +0400

    Исправления в окне авторизации
    Исправлено поле "Страна", теперь это ComboBox с подготовленными значениями.
    Исправлено положение заглавной надписи "Shop Database", теперь на всех сценах она имеет одинаково ровное положение

--- src/sample/controllers/SignUpController.java
+++ src/sample/controllers/SignUpController.java
@@ -3,14 +3,14 @@ package sample.controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import sample.dataBase.DataBaseHandler;
@@ -39,7 +39,7 @@ public class SignUpController {
     private TextField signUpPassword;
 
     @FXML
-    private TextField signUpCountry;
+    private ComboBox<String> signUpCountry;
 
     @FXML
     private RadioButton signUpMale;
@@ -53,6 +53,7 @@ public class SignUpController {
     @FXML
     private ImageView signUpBackIcon;
 
+    ObservableList<String> counties = FXCollections.observableArrayList("Россия", "Не Россия", "Уганда");
     @FXML
     void initialize() {
         signUpButton.setOnAction(event -> {
@@ -60,9 +61,17 @@ public class SignUpController {
             System.out.println("SUCCESS REGISTRATION");
             signUpButton.getScene().getWindow().hide();
         });
-        signUpBackIcon.setOnMouseClicked(event -> {
-            openNewScene("/sample/view/loginPage.fxml");
-        });
+        signUpBackIcon.setOnMouseClicked(event -> openNewScene("/sample/view/loginPage.fxml"));
+        signUpCountry.setValue("Россия");
+        signUpCountry.setItems(counties);
+    }
+
+    public ObservableList<String> getCounties() {
+        return counties;
+    }
+
+    public void setCounties(ObservableList<String> counties) {
+        this.counties = counties;
     }
 
     private void signUpNewUser() {
@@ -71,7 +80,7 @@ public class SignUpController {
         String lastname = signUpLastname.getText();
         String username = signUpUsername.getText();
         String password = signUpPassword.getText();
-        String location = signUpCountry.getText();
+        String location = String.valueOf(signUpCountry.getValue());
         String gender = "";
         if (signUpMale.isSelected()) {
             gender = "Мужской";
